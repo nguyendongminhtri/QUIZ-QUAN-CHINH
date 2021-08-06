9e1bedd352bd27e5a480942722c257382e3b81b9
nguyendongminhtri <73800938+nguyendongminhtri@users.noreply.github.com>
2021-08-06 19:06:03 +0700

    avatar mac dinh

--- src/main/java/com/example/demo/dto/response/JwtResponse.java
+++ src/main/java/com/example/demo/dto/response/JwtResponse.java
@@ -9,27 +9,37 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private String name;
+    private String avatar;
     private Collection<? extends GrantedAuthority> roles;
 
     public JwtResponse() {
     }
 
-    public JwtResponse(Long id, String token, String type, String name, Collection<? extends GrantedAuthority> roles) {
+    public JwtResponse(Long id, String token, String type, String name, String avatar,Collection<? extends GrantedAuthority> roles) {
         this.id = id;
         this.token = token;
         this.type = type;
         this.name = name;
+        this.avatar = avatar;
         this.roles = roles;
     }
 
-    public JwtResponse(String token, String name, Collection<? extends GrantedAuthority> authorities) {
+    public JwtResponse(String token, String name, String avatar, Collection<? extends GrantedAuthority> authorities) {
         this.token = token;
         this.name = name;
+        this.avatar = avatar;
         this.roles = authorities;
     }
-
     public Long getId() {
         return id;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getAvatar() {
+        return avatar;
     }
 
     public void setId(Long id) {
